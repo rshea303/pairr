5fd251ae8888187b19bf975c7d5b26a359940221
Rich Shea <richcshea@gmail.com>
2015-06-23 15:54:32 -0600

    new user is directed to user info page to add languages and description

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,23 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+    if User.exists?(uid: request.env['omniauth.auth'].uid)
+      user = User.find_or_create_from_auth(request.env['omniauth.auth'])
 
-    if user
-      session[:user_id] = user.id
-      redirect_to dashboard_path
+      if user
+        session[:user_id] = user.id
+        redirect_to dashboard_path
+      else
+        redirect_to root_path
+      end
+
     else
-      redirect_to root_path
+      user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      if user
+        session[:user_id] = user.id
+        redirect_to edit_user_path(user)
+      else
+        redirect_to root_path
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: "sessions#create"
   get '/dashboard', to: "static_pages#dashboard"
   root 'static_pages#home'
+
+  resources :users
 end

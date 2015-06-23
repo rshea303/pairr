76046ff7c3773329873a4e26f9f0b40fcdb6c3ba
Rich Shea <richcshea@gmail.com>
2015-06-23 17:51:56 -0600

    add checklist and description functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,34 @@ class UsersController < ApplicationController
 
   def update
     user = User.find_by(id: params[:id])
-    languages = Language.find_by
-    if user.update(user_params) 
-      redirect_to edit_path_user(user)
+
+    if params[:language_ids]
+      language_ids = params[:language_ids].values
+      language_ids.each do |id|
+        user.languages << Language.find(id)
+      end
+      user.save
+
+      if user.description.nil?
+        flash[:alert] = "Please complete the description"
+        redirect_to edit_user_path(user)
+      else
+        flash[:notice] = "Let's get pairing"
+        redirect_to dashboard_path
+      end
     else
-      render :edit
+      new_description = params[:description]
+      user.description = new_description
+      user.save
+      
+      if user.languages.nil?
+        flash[:alert] = "Please select some languages"
+        redirect_to edit_user_path(user)
+      else
+        flash[:notice] = "Let's get pairing"
+        redirect_to dashboard_path
+      end
     end
+
   end
 end

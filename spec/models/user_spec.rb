824af14a79bde40cc07c6f6a49abfdf46c0726d7
Rich Shea <richcshea@gmail.com>
2015-06-23 16:05:47 -0600

    user can have description

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can have a description" do
+    user = User.new(description: "no more than 500 words")
+    
+    expect(user).to be_valid
+  end
 end

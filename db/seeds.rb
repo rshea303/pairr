3f345622264a5d2eadc518e58138b9e31132a622
Rich Shea <richcshea@gmail.com>
2015-06-23 18:49:57 -0600

    add users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
 class Seed
+
   def languages
     ["Ruby", "JavaScript", "VisualBasic", "Python", "Java"].each do |language|
       Language.create(name: language)
     end
   end
+
+  def users
+    50.times do |i|
+    user = User.create(nickname: "funny name #{i}",
+                uid: "100#{i}",
+                image_url: "https://avatars.githubusercontent.com/u/7582765?v=3",
+                description: "This is my description.")
+    user.languages << Language.find(rand(1..3))
+    user.languages << Language.find(rand(4..5))
+    user.save
+    end
+  end
 end
 
-Seed.new.languages
+run = Seed.new
+run.languages
+run.users
